46b6852e2a4c6333c271e06078c2c612b0f4c8c2
Mohammed Gamal <mohammed_gamal7@outlook.com>
2023-05-17 15:25:50 +0300

    update

--- back/C#/6/School/School/Program.cs
+++ back/C#/6/School/School/Program.cs
@@ -7,20 +7,32 @@
             Student s1 = new Student(1, "Mohammed");
             Student s2 = new Student(2, "Gamal");
             Student s3 = new Student(3, "Ahmed");
+            Student s4 = new Student(4, "Khaled");
+            Student s5 = new Student(5, "Tarek");
 
-            Department Dep1 = new Department(1, "dep1");
-            Dep1.AddStudent(s1);
-            Dep1.AddStudent(s2);
-            Dep1.AddStudent(s3);
+            //*************************** Department One *********************************//
 
-            Console.WriteLine(Dep1);
-            Console.WriteLine("*************************");
+            Department departOne = new Department(1, "SE");
+            departOne.AddStudent(s1);
+            departOne.AddStudent(s2);
+            departOne.AddStudent(s3);
+
             s2.setAbsent(2);
             s3.Absentdays = 1;
             s3.Absentdays = 7;
-            Console.WriteLine(Dep1);
-            Console.WriteLine("*************************");
+            Console.WriteLine(departOne);
             Console.WriteLine(s3);
+            Console.WriteLine("******************************");
+
+            //*************************** Department Two *********************************//
+            Department departTwo = new Department(2, "IT");
+            departTwo.AddStudent(s4);
+            departTwo.AddStudent(s5);
+
+            s4.setAbsent(2);
+            s5.Absentdays = 6;
+            Console.WriteLine(departTwo);
+            Console.WriteLine(s5);
 
             Console.ReadKey();
         }
